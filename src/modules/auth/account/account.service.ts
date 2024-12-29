2e8836d6912091103328117e711659fbc159d49e
nh40 <imbovoebozestvo@gmail.com>
2024-12-29 14:19:02 +0700

    control for sessions of auth

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,14 +1,56 @@
 import { Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
 
 import { PrismaService } from '@/src/core/prisma/prisma.service'
+
+import { CreateUserInput } from './inputs/create-user.input'
 
 @Injectable()
 export class AccountService {
 	public constructor(private readonly prismaService: PrismaService) {}
 
-	public async findAll() {
-		const users = await this.prismaService.user.findMany()
+	public async me(id: string) {
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				id
+			}
+		})
 
-		return users
+		return user
+	}
+
+	public async create(input: CreateUserInput) {
+		const { username, email, password } = input
+
+		const isUserNameExists = await this.prismaService.user.findUnique({
+			where: {
+				username
+			}
+		})
+
+		if (isUserNameExists) {
+			throw new Error('Это имя пользователя уже занято')
+		}
+
+		const isEmailExists = await this.prismaService.user.findUnique({
+			where: {
+				email
+			}
+		})
+
+		if (isEmailExists) {
+			throw new Error('Пользователь с этим email уже зарегистрирован')
+		}
+
+		await this.prismaService.user.create({
+			data: {
+				username,
+				email,
+				password: await hash(password),
+				displayName: username
+			}
+		})
+
+		return true
 	}
 }
